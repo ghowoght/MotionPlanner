b01e8e1c60e5932af3366d57930d45548cd6d323
木士 鬼 <2550319354@qq.com>
2021-01-01 14:39:37 +0800

    添加图数据结构类，主要用于采样算法

---
+++ MotionPlanner/Graph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionPlanner
+{
+    public class Node : IComparable<Node>
+    {
+        public int x = 0;
+        public int y = 0;
+        public double cost = 0;
+        public List<Node> neighbor = new List<Node>();
+        public Node front = null;
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int CompareTo(Node other)
+        {
+            return this.cost < other.cost ? -1 : (this.cost == other.cost ? 0 : 1);
+        }
+
+        public bool isEqual(Node other) // 判断输入结点和本结点是否相同
+        {
+            if (this.x == other.x && this.y == other.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+    public class Graph
+    {
+        public List<Node> nodes = new List<Node>();
+    }
+}
